3a42c0d21c8882ee01dba5fba6184cd04feb2bb5
Luke Horvat <lukehorvat@gmail.com>
2016-03-18 16:21:40 +1100

    Disable checks when user hasn't defined any dependencies

--- lib/path-dependencies/railtie.rb
+++ lib/path-dependencies/railtie.rb
@@ -1,6 +1,8 @@
 module PathDependencies
   class Railtie < Rails::Railtie
     config.before_initialize do
+      next unless config.respond_to?(:path_dependencies)
+
       config.path_dependencies.select do |dependency|
         !system("which #{dependency} &>/dev/null")
       end.each do |dependency|
